4f1325fc9d5ed7219578946a37f6d5a47f6f9609
Eniola Samuel <theeniolasamuel@gmail.com>
2024-04-04 20:56:55 +0100

    first tcc commit

---
+++ src/views/NavBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import "../styles/NavBar.css"
+
+const NavBar =() =>{
+
+    const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1,
+            slidesToSlide: 1,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+            slidesToSlide: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+            slidesToSlide: 1,
+        },
+    };
+
+    let links = [
+        { name: "Home", link: "/" },
+        { name: "Services", link: "/" },
+        { name: "About Us", link: "/" },
+        { name: "Testimonials", link: "/" },
+        { name: "Contact Us", link: "/" },
+        { name: "FAQs", link: "/" },
+    ];
+    return(
+        <div className="navigation-main-frame">
+
+        </div>
+    )
+}
