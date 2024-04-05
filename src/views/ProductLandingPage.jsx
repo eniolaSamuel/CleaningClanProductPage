bb6f7779f9fb119462930b1f1437e5d4432530af
Eniola Samuel <theeniolasamuel@gmail.com>
2024-04-05 21:28:15 +0100

    subsequent tcc commit

---
+++ src/views/ProductLandingPage.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./NavBar";
+
+const ProductLandingPage =() =>{
+    return(
+        <div>
+            <NavBar />
+        </div>
+    )
+}
+
+export default ProductLandingPage;
