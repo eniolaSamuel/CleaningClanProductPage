7ffb2ac345685d10e1f0a396363ef60fdc1ab5fd
Eniola Samuel <theeniolasamuel@gmail.com>
2024-04-11 21:20:01 +0100

    subsequent tcc commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./views/NavBar";
 import HomePage from "./views/HomePage";
 import {ContactUs} from "./views/Contact";
+import ProductLandingPage from "./views/ProductLandingPage";
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <>
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<HomePage />} />
+            <Route path="/" element={<ProductLandingPage/>} />
             <Route path="/navigation-bar" element={<NavBar />} />
             <Route path="/cpontact" element={<ContactUs />} />
           </Routes>
