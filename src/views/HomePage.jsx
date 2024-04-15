b8561bcc6175e65764c19c6d5baa5ba2d69153dd
Eniola Samuel <theeniolasamuel@gmail.com>
2024-04-15 22:05:24 +0100

    subsequent tcc commit

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -1,4 +1,5 @@
 import "../styles/HomePage.css"
+import CLEANINGPERSONS from "../assets/svg/cleaning-personel.svg"
 import NavBar from "./NavBar";
 import {ContactUs} from "./Contact";
 
@@ -6,7 +7,9 @@ const HomePage = () =>{
     return(
         <div className="home-page-main-frame">
             <div className="text-frame"></div>
-            <div className="side-image"></div>
+            <div className="side-image">
+                <img src={CLEANINGPERSONS} alt={CLEANINGPERSONS}/>
+            </div>
 
         </div>
     )
