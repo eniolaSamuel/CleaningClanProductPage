af66fcbd7b585ee32732ca7e7e92aed90dc37030
Eniola Samuel <theeniolasamuel@gmail.com>
2024-04-24 22:27:28 +0100

    subsequent tcc commit

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -8,7 +8,9 @@ const HomePage = () =>{
         <div className="home-page-main-frame">
             <div className="text-frame">
                 <h1>Welcome to The Cleaning Clan!</h1>
-                <p></p>
+                <p>We're your clan for clean spaces. From homes to offices,
+                    we've got your cleaning needs covered.
+                    Let us bring the sparkle back to your space. Book now and join the clan</p>
             </div>
             <div className="side-image">
                 <img src={CLEANINGPERSONS} alt={CLEANINGPERSONS}/>
