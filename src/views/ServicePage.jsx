283ad830812e432cfb1432e6078ae36aa08e0618
Eniola Samuel <theeniolasamuel@gmail.com>
2024-05-09 21:08:25 +0100

    subsequent tcc commit

---
+++ src/views/ServicePage.jsx
@@ -0,0 +1,13 @@
+const ServicePage = () => {
+    return(
+        <div className="service-main-frame">
+            <div>
+                <h1>Expert Cleaning Solutions for Sparkling, Spotless Spaces</h1>
+            </div>
+            <div>
+
+            </div>
+            <div></div>
+        </div>
+    )
+}
