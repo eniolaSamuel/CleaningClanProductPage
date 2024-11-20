f27a11baefd6e19a47d0d484de7a4a6d42931743
Eniola Samuel <theeniolasamuel@gmail.com>
2024-11-20 15:41:36 +0100

    cc-update

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,21 +1,50 @@
-import React, { useRef } from "react";
+import React, {useRef, useState} from "react";
 import emailjs from "emailjs-com";
 import "../styles/Contact.css";
 import Mail from "../assets/svg/icons8-send-mail.svg";
 import CellPhone from "../assets/svg/icons8-cell-phone.svg";
 import Location from "../assets/svg/icons8-location.svg";
 
+
 export const ContactUs = () => {
     const form = useRef();
+    const [loading, setLoading] = useState(false);
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        service: "",
+        message: "",
+    });
+    const handleInputChange = (name, value) => {
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    };
 
     const sendEmail = (e) => {
         e.preventDefault();
 
+        const validationErrors = {};
+        if (!formData.name) validationErrors.name = "Name is required";
+        if (!formData.service) validationErrors.service = "Please select a service.";
+        if (!formData.email) validationErrors.email = "Email is required";
+        if (!formData.message) validationErrors.phone = "Additional message is required";
+ 
+
+
+        setLoading(true);
+        const customMessage = `
+        Name: ${formData.name}
+        Email: ${formData.email}
+        Service Selected: ${formData.service}
+        Message: ${formData.message}
+    `;
+
         emailjs
-            .sendForm(
+            .send(
                 "service_2kvfw6b",
                 "template_zl3djr3",
-                form.current,
+                {
+                    custom_message: customMessage,
+                },
                 "W6IqnvBD7h6TUXe3b"
             )
             .then(
@@ -28,8 +57,10 @@ export const ContactUs = () => {
                     alert("Failed to send message, please try again.");
                 }
             );
+
         e.target.reset();
     };
+
 
 
     return (
@@ -43,13 +74,34 @@ export const ContactUs = () => {
 
                 <form ref={form} onSubmit={sendEmail}>
                     <label htmlFor="name" className="name-label">Your Name</label>
-                    <input id="name" type="text" name="user_name" placeholder="John Doe" required/>
+                    <input
+                        id="name"
+                        type="text"
+                        name="user_name"
+                        placeholder="John Doe"
+                        value={formData.name}
+                        onChange={(e) => handleInputChange("user_name", e.target.value)}
+                        required
+                    />
 
                     <label htmlFor="email" className="email-label">Email</label>
-                    <input id="email" type="email" name="user_email" placeholder="example@email.com" required/>
+                    <input
+                        id="email"
+                        type="email"
+                        name="user_email"
+                        placeholder="example@email.com"
+                        value={formData.email}
+                        onChange={(e) => handleInputChange("user_email", e.target.value)}
+                        required
+                    />
 
                     <label htmlFor="service" className="service-label">Select Service</label>
-                    <select id="service" name="user_service" required>
+                    <select
+                        id="service"
+                        name="user_service"
+                        onChange={(e) => handleInputChange("service", e.target.value)}
+                        required
+                    >
                         <option value="" disabled selected>Select a service</option>
                         <option value="Residential Cleaning">Residential Cleaning</option>
                         <option value="Commercial Cleaning">Commercial Cleaning</option>
@@ -62,7 +114,9 @@ export const ContactUs = () => {
 
                     <label htmlFor="message" className="message-label">Additional Message</label>
                     <textarea id="message" name="message"
-                              placeholder="Input additional message including property description and number of rooms" required></textarea>
+                              placeholder="Input additional message including property description and number of rooms"
+                              value={formData.message}
+                              required></textarea>
 
                     <button type="submit">Send Message</button>
                 </form>
