ecaeb78b4a4cf0f860c0b25875235ddbbca09c40
Eniola Samuel <theeniolasamuel@gmail.com>
2024-11-25 14:28:28 +0100

    cc-update

--- src/views/ProductLandingPage.jsx
+++ src/views/ProductLandingPage.jsx
@@ -12,8 +12,8 @@ const ProductLandingPage =() =>{
             <HomePage/>
             <ServicePage />
             <About />
-            {/*<Value />*/}
             <ContactUs />
+
         </div>
     )
 }
